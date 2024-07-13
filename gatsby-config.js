ef9e08d2e4127977daf111cde90ace2c12393a49
Akash Kumar <akash.kumar@arkasoftwares.com>
2024-07-13 16:21:28 +0530

    project set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `hssindia`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-postcss"]
 };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from './src/Layout';
-import './src/styles/global.css'
 
 // Wrap every page element with a Layout component
 export const wrapPageElement = ({ element, props }) => {
