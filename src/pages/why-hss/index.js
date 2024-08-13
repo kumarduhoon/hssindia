7e16f50a3a7a64276891ae3ae413ae56fa0582c8
Akash Kumar <akash.kumar@arkasoftwares.com>
2024-08-14 00:21:48 +0530

    Design : why hss page designed

--- src/pages/why-hss/index.js
+++ src/pages/why-hss/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import WhyHSSHero from "../../components/Why_HSS/WhyHssHero/WhyHssHeros"
 import WhyHssCards from "../../components/Why_HSS/WhyHssCards/WhyHssCards"
+import WhyHssVerticalCard from "../../components/Why_HSS/whyHssVertical/WhyHssVerticalCard"
 
 function WhyHss() {
   return (
     <>
       <WhyHSSHero />
       <WhyHssCards />
+      <WhyHssVerticalCard />
+      <WhyHssCards />
+      <div className='mb-8'></div>
     </>
   )
 }
