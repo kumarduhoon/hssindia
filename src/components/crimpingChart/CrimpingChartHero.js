5cfe90a5ac0723293854be594791f00f7f784c74
Akash Kumar <akash.kumar@arkasoftwares.com>
2024-08-17 04:41:15 +0530

    Design: page Design

---
+++ src/components/crimpingChart/CrimpingChartHero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+
+
+function CrimpingChartHero() {
+  return (
+    <StaticImage
+      src="../../../static/Crimping_Chart.webp"
+      alt='Crimping Chart'
+    />
+  )
+}
+
+export default CrimpingChartHero
---
+++ src/components/crimpingChart/CrimpingChartTable.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+
+function CrimpingChartTable() {
+  return (
+    <div className='flex justify-center my-10'>
+      <StaticImage
+        src='../../../static/chart.webp'
+        alt='chart'
+      />
+    </div>
+
+  )
+}
+
+export default CrimpingChartTable
