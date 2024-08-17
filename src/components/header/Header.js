59ab8b25bc2dc67a2f286c02d7ad83d21763c05c
Akash Kumar <akash.kumar@arkasoftwares.com>
2024-08-18 04:29:08 +0530

    Design: market page design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -32,9 +32,52 @@ function Header() {
                         <b>
                             <Link className="header-text-color text-xl font-bold cursor-pointer underline-animation">Why HSS</Link>
                         </b>
-                        <b>
-                            <Link className="header-text-color text-xl font-bold cursor-pointer underline-animation">Techical</Link>
-                        </b>
+                        <div className=" group relative cursor-pointer">
+                            <div className="flex items-center justify-between space-x-2 px-4">
+                                <b>
+                                    <Link className="header-text-color text-xl font-bold cursor-pointer menu-hover">Techical</Link>
+                                </b>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="white" class="h-6 w-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </span>
+                            </div>
+
+                            <div
+                                class="invisible absolute z-50 flex flex-col bg-gray-100 py-1 px-4 text-gray-800 shadow-xl group-hover:visible left-6">
+
+                                <a class="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                                    Sunday
+                                </a>
+
+                                <a class="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                                    Monday
+                                </a>
+
+                                <a class="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                                    Tuesday
+                                </a>
+
+                                <a class="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                                    Wednesday
+                                </a>
+
+                                <a class="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                                    Thursday
+                                </a>
+
+                                <a class="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                                    Friday
+                                </a>
+
+                                <a class="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                                    Saturday
+                                </a>
+
+                            </div>
+                        </div>
                         <b>
                             <Link className="header-text-color text-xl font-bold cursor-pointer underline-animation">Reach us</Link>
                         </b>
