b9c2f7b6f378e26c98190e3817d066b4c0b7bcb4
Akash Kumar <akash.kumar@arkasoftwares.com>
2025-03-08 16:37:00 +0530

    remove un wanted code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import Layout from './src/Layout';
-import Alpine from 'alpinejs';
 import './src/styles/global.css'
-import 'swiper/css'
 
-// Wrap every page element with a Layout component
 export const wrapPageElement = ({ element, props }) => {
     return <Layout {...props}>{element}</Layout>;
-};
-
-
-
-export const onClientEntry = () => {
-    window.Alpine = Alpine;
-    Alpine.start();
 };
